1cc0068eaff82ac1b06b6c113e4b361b4086b83f
aijun.sun <aijun.sun@spreadtrum.com>
2013-01-24 17:07:11 +0800

    Bug#000 modify proprietories.mk of Garda and MINT
    Change-Id: I457bd040fd03cba0d1d31d18c8865d4d18fed95d

--- mint/proprietories.mk
+++ mint/proprietories.mk
@@ -1,12 +1,7 @@
 
-ifneq ($(shell ls -d vendor/sprd/proprietories-source 2>/dev/null),)
 # for spreadtrum internal proprietories modules: only support package module names
 
-OPENCORE :=  libopencore_common libomx_sharedlibrary libomx_m4vdec_sharedlibrary libomx_m4venc_sharedlibrary \
-	libomx_avcdec_sharedlibrary pvplayer.cfg
-
 PRODUCT_PACKAGES := \
-	$(OPENCORE) \
 	rild_sp \
 	libril_sp \
 	libreference-ril_sp \
@@ -14,7 +9,6 @@ PRODUCT_PACKAGES := \
 	phoneserver \
 	phoneserver_2sim
 
-else
 # for spreadtrum customer proprietories modules: only support direct copy
 
 PROPMODS := \
@@ -24,13 +18,6 @@ PROPMODS := \
 	system/lib/libomx_m4venc_sharedlibrary.so \
 	system/lib/libomx_avcdec_sharedlibrary.so \
 	system/etc/pvplayer.cfg \
-	system/bin/rild_sp \
-	system/lib/libril_sp.so \
-	system/lib/libreference-ril_sp.so \
-	system/bin/sprd_monitor \
-	system/bin/phoneserver \
-	system/bin/phoneserver_2sim
 
-PRODUCT_COPY_FILES := $(foreach f,$(PROPMODS),vendor/sprd/proprietories/cori2g/$(f):$(f))
+PRODUCT_COPY_FILES := $(foreach f,$(PROPMODS),vendor/sprd/proprietories/mint/$(f):$(f))
 
-endif
