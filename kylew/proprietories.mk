18548308e1cba4424857b8b16988000bd2b4a438
aijun.sun <aijun.sun@spreadtrum.com>
2013-01-25 13:27:22 +0800

    Bug#000 modify proprietories.mk of kylew and kyletd
    Change-Id: I36a2d74a81d7a51a782780d2ad391d9ba8d80d40

--- kylew/proprietories.mk
+++ kylew/proprietories.mk
@@ -1,19 +1,21 @@
 
-ifneq ($(shell ls -d vendor/sprd/proprietories-source 2>/dev/null),)
-# for spreadtrum internal proprietories modules: only support package module names
-
-OPENCORE :=  libopencore_common libomx_sharedlibrary libomx_m4vdec_sharedlibrary libomx_m4venc_sharedlibrary \
-	libomx_avcdec_sharedlibrary pvplayer.cfg
-
 PRODUCT_PACKAGES := \
-	$(OPENCORE) \
 	rild_sp \
 	libril_sp \
 	libreference-ril_sp \
 	sprd_monitor \
 	phoneserver \
-	phoneserver_2sim \
-	akmd8975
+	phoneserver_2sim
+
+
+ifneq ($(shell ls  vendor/sprd/proprietories/kylew/prop.list 2>/dev/null),)
+# for spreadtrum internal proprietories modules: only support package module names
+
+OPENCORE :=  libopencore_common libomx_sharedlibrary libomx_m4vdec_sharedlibrary libomx_m4venc_sharedlibrary \
+	libomx_avcdec_sharedlibrary pvplayer.cfg
+
+PRODUCT_PACKAGES += \
+	$(OPENCORE)
 
 else
 # for spreadtrum customer proprietories modules: only support direct copy
@@ -24,14 +26,7 @@ PROPMODS := \
 	system/lib/libomx_m4vdec_sharedlibrary.so \
 	system/lib/libomx_m4venc_sharedlibrary.so \
 	system/lib/libomx_avcdec_sharedlibrary.so \
-	system/etc/pvplayer.cfg \
-	system/bin/rild_sp \
-	system/lib/libril_sp.so \
-	system/lib/libreference-ril_sp.so \
-	system/bin/sprd_monitor \
-	system/bin/phoneserver \
-	system/bin/phoneserver_2sim \
-	system/bin/akmd8975
+	system/etc/pvplayer.cfg
 
 PRODUCT_COPY_FILES := $(foreach f,$(PROPMODS),vendor/sprd/proprietories/kylew/$(f):$(f))
 
