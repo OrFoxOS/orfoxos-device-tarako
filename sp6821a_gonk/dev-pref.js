41e39f281b709d95983812fb6e8f2c2432beb7f2
james.zhang <james.zhang@spreadtrum.com>
2014-04-08 16:59:28 +0800

    Bug#269156 revert b2g.js change
    [bug number  ]
    [root cause  ]
    [changes     ]
    [side effects]
    [self test   ] yes
    [reviewers   ]
    
    Change-Id: I145617b134cecbe0ece3d36a3fc9901731e25624

--- sp6821a_gonk/dev-pref.js
+++ sp6821a_gonk/dev-pref.js
@@ -7,20 +7,6 @@ pref("dom.ipc.processPriorityManager.backgroundLRUPoolLevels", 2);
 // Number of background process would have a hard limit according to the
 // calculation result from backgroundLRUPoolLevels argument.
 pref("hal.processPriorityManager.gonk.enableOOBPKiller", true);
- 
-pref("hal.processPriorityManager.gonk.MASTER.OomScoreAdjust", -1000);
-
-pref("hal.processPriorityManager.gonk.FOREGROUND_HIGH.KillUnderKB", 2048);
-
-pref("hal.processPriorityManager.gonk.FOREGROUND.KillUnderKB", 4096);
-
-pref("hal.processPriorityManager.gonk.BACKGROUND_PERCEIVABLE.KillUnderKB", 8172);
-
-pref("hal.processPriorityManager.gonk.BACKGROUND_HOMESCREEN.KillUnderKB", 16384);
-
-pref("hal.processPriorityManager.gonk.BACKGROUND.KillUnderKB", 18432);
-
-pref("hal.processPriorityManager.gonk.notifyLowMemUnderKB", 14336);
 
 // Geolocation configuration to use the Mozilla Location Service.
 pref("geo.provider.use_mls", true);
